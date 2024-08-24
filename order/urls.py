0f47926cc605d76f471a2f686ef727afbb24c3d1
bunyod <naimov.bunyod2021@gmail.com>
2024-08-24 19:14:32 +0500

    delete directory all_media_files

--- order/urls.py
+++ order/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from order.views import AddCartItemView, CartItemListView, RemoveCartItemView, OrderCreateView
 
 urlpatterns = [
+    path('', OrderCreateView.as_view(), name='order_create'),
     path('cart-item-list', CartItemListView.as_view(), name='cart_item_list'),
     path('add-cart/<int:product_id>/', AddCartItemView.as_view(), name='add_cart_item'),
     path('remove-cart-item/<int:product_id>/', RemoveCartItemView.as_view(), name='remove_cart_item'),
-    path('create/', OrderCreateView.as_view(), name='order_create'),
 ]
