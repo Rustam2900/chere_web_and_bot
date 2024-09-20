6a7bd32d0ef1d7fa09980b85c70df6eaa1648612
bunyod <naimov.bunyod2021@gmail.com>
2024-09-20 17:22:10 +0500

    fix staticfiles

--- core/settings/base.py
+++ core/settings/base.py
@@ -181,12 +181,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
 if DEBUG:
     STATICFILES_DIRS = [
-        BASE_DIR.joinpath('staticfiles'),
-    ]
-STATIC_ROOT = BASE_DIR / 'static'
+        BASE_DIR.joinpath('staticfiles'),]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
